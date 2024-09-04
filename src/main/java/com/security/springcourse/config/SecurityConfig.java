a187113ec9b2d22e94ccae13110c4153be774255
Kravtsev Artem <arkravt@gmail.com>
2024-09-04 16:25:43 +0400

    04.09.2024

--- src/main/java/com/security/springcourse/config/SecurityConfig.java
+++ src/main/java/com/security/springcourse/config/SecurityConfig.java
@@ -3,10 +3,15 @@ package com.security.springcourse.config;
 import com.security.springcourse.services.PersonDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.ldap.EmbeddedLdapServerContextSourceFactoryBean;
+import org.springframework.security.config.ldap.LdapBindAuthenticationManagerFactory;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -21,18 +26,24 @@ public class SecurityConfig {
 //        return new PersonDetailsService();
 //    }
 //
-//    @Bean
+//    @Bean // Ставим степень кодировки, с которой кодировали пароль в базе
+//    public PasswordEncoder passwordEncoder() {
+//        //return new BCryptPasswordEncoder(5);
+//        return NoOpPasswordEncoder.getInstance();
+//    }
+//
+//    @Bean // Определяем AuthenticationProvider по умолчанию если нету своего кастомного
 //    public AuthenticationProvider authenticationProvider() {
 //        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
 //        provider.setUserDetailsService(userDetailsService());
 //        provider.setPasswordEncoder(passwordEncoder());
 //        return provider;
 //    }
-//
-//    @Bean // Ставим степень кодировки, с которой кодировали пароль в базе
-//    public PasswordEncoder passwordEncoder() {
-//        //return new BCryptPasswordEncoder(5);
-//        return NoOpPasswordEncoder.getInstance();
+
+
+//    @Bean
+//    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+//        return configuration.getAuthenticationManager();
 //    }
 
 }
