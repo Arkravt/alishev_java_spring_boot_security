0c00750f09902019557dd89a73d63b1708c7df08
utr028 <arkravt@gmail.com>
2024-09-05 16:10:17 +0400

    05.09.2024

--- src/main/java/com/security/springcourse/config/SecurityConfig.java
+++ src/main/java/com/security/springcourse/config/SecurityConfig.java
@@ -1,49 +1,75 @@
 package com.security.springcourse.config;
 
+import com.security.springcourse.repositories.PeopleRepository;
 import com.security.springcourse.services.PersonDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.config.ldap.EmbeddedLdapServerContextSourceFactoryBean;
 import org.springframework.security.config.ldap.LdapBindAuthenticationManagerFactory;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
 
-//    @Bean // Возвращаем кастомный PersonDetailsService, который напишем далее
-//    public UserDetailsService userDetailsService() {
-//        return new PersonDetailsService();
-//    }
-//
-//    @Bean // Ставим степень кодировки, с которой кодировали пароль в базе
-//    public PasswordEncoder passwordEncoder() {
-//        //return new BCryptPasswordEncoder(5);
-//        return NoOpPasswordEncoder.getInstance();
-//    }
-//
+    private final PersonDetailsService personDetailsService;
+
+    @Autowired
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable) // Отключаем защиту от межсайтовой подделки запросов
+                .authorizeHttpRequests(authrize -> authrize
+                        .requestMatchers("/auth/login", "/error").permitAll() // только эти запросы разрешаем для не авторизованных пользователей.
+                        .anyRequest().authenticated() // для всех остальных запросов пользователь должен быть аутентифицирован.
+                )
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/auth/login") // указываем адрес нашей странички логина.
+                        .loginProcessingUrl("/process_login") // указываем куда передаем данные с формы.
+                        .defaultSuccessUrl("/hello", true) // указываем страницу при успешной аутентификации.
+                        .failureUrl("/auth/login?error")); // указываем страницу при не верной аутентификации.
+
+        return http.build();
+
+    }
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(personDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        //return new BCryptPasswordEncoder(5);
+        return NoOpPasswordEncoder.getInstance();
+    }
+
 //    @Bean // Определяем AuthenticationProvider по умолчанию если нету своего кастомного
 //    public AuthenticationProvider authenticationProvider() {
 //        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-//        provider.setUserDetailsService(userDetailsService());
+//        provider.setUserDetailsService(personDetailsService);
 //        provider.setPasswordEncoder(passwordEncoder());
 //        return provider;
-//    }
-
-
-//    @Bean
-//    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-//        return configuration.getAuthenticationManager();
 //    }
 
 }
--- src/main/java/com/security/springcourse/services/PersonDetailsService.java
+++ src/main/java/com/security/springcourse/services/PersonDetailsService.java
@@ -16,25 +16,19 @@ import java.util.Optional;
 @Transactional(readOnly = true)
 public class PersonDetailsService implements UserDetailsService  {
 
-    @Autowired
     private PeopleRepository peopleRepository;
 
-//    @Autowired
-//    public PersonDetailsService(PeopleRepository peopleRepository) {
-//        this.peopleRepository = peopleRepository;
-//    }
+    @Autowired
+    public PersonDetailsService(PeopleRepository peopleRepository) {
+        this.peopleRepository = peopleRepository;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Optional<Person> person = peopleRepository.findByUsername(username);
 
         if(person.isEmpty())
-            throw new UsernameNotFoundException("User not found !");
-
-//        return new org.springframework.security.core.userdetails.User(
-//                person.get().getUsername(),
-//                person.get().getPassword(),
-//                null);
+            throw new UsernameNotFoundException("User not found !!");
 
         return new PersonDetails(person.get());
     }
